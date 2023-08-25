33f88d27e18790efa25ebd229af434d417e5b1de
Augustus Kling <augustus.kling@gmail.com>
2023-08-25 21:00:53 +0200

    Reduce probability of repeating learned sentences.

--- src/main.tsx
+++ src/main.tsx
@@ -153,14 +153,23 @@ function App() {
         const amountToShow = Math.min(10, exercisePicklist.length);
         const picklistCopy = [...(exercisesByStatus.wrong || []), ...(exercisesByStatus.somewhat || [])];
         const picked: string[] = [];
+        // 70 % wrong or somewhat mastered sentences.
         while(picked.length < Math.min(amountToShow*0.7, picklistCopy.length)) {
             const pickedIndex = Math.floor(Math.random() * picklistCopy.length);
             picked.push(...picklistCopy.splice(pickedIndex, 1));
         }
+        // 30 % unseen sentences.
         while(picked.length < amountToShow && exercisesByStatus.unseen?.length > 0) {
             const pickedIndex = Math.floor(Math.random() * exercisesByStatus.unseen.length);
             picked.push(...exercisesByStatus.unseen.splice(pickedIndex, 1));
         }
+        // Fill up to 100% with everything not masteded.
+        const everythingButMastered = picklistCopy.concat(exercisesByStatus.unseen);
+        while(picked.length < Math.min(amountToShow, everythingButMastered.length)) {
+            const pickedIndex = Math.floor(Math.random() * everythingButMastered.length);
+            picked.push(...everythingButMastered.splice(pickedIndex, 1));
+        }
+        // If 100% still not reached pick any sentence from exercise.
         const unpickedSoFar = exercisePicklist.filter(e => !picked.includes(e));
         while(picked.length < amountToShow) {
             const pickedIndex = Math.floor(Math.random() * unpickedSoFar.length);
