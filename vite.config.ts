616a4adc243c073c15a7bbbbb17646915288d99c
Augustus Kling <augustus.kling@gmail.com>
2023-09-07 22:51:13 +0200

    PWA offline mode.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
+import { VitePWA } from 'vite-plugin-pwa'
+
 /** @type {import('vite').UserConfig} */
 export default {
   build: {
     target: "es2022",
     outDir: "docs"
   },
-  base: '/lingo/'
+  base: '/lingo/',
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: [
+          '**/*.{js,css,html,png,svg,txt}',
+          'dist-data/courses/index.json'
+        ]
+      },
+      manifest: {
+        name: 'lingo',
+        short_name: 'lingo',
+        description: 'Vocabulary trainer based on Tatoeba data',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa/logo.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ]
 }
